ab0df11cc530b6c3b9ff7d3ca01133e28f3def66
Nikolaev <n.nikolaevbg97@gmail.com>
2022-07-14 20:57:33 +0300

    Add cache time functionallity and refactored

--- homeworks/third_homework/users_cache/lib/models/user.ex
+++ homeworks/third_homework/users_cache/lib/models/user.ex
@@ -1,21 +1,38 @@
 defmodule Models.User do
-  defstruct [:username, :avatar_url, :bio, :created_at, :email, :id]
+  defstruct [
+    :username,
+    :avatar_url,
+    :bio,
+    :created_at,
+    :email,
+    :id,
+    :cached_at
+  ]
 
-  def contains([], _username),
-    do: false
+  # Returns user if exists, otherwise returns nil.
+  def find(users, username) do
+    user = Enum.filter(users, fn user -> user.username === username end)
+    do_find(user)
+  end
 
-  # def contains([user], username),
-  #   do: contains([], username, user.username)
+  # Checks if user cache is old.
+  def old_cache?(%__MODULE__{cached_at: cached_at}, cached_time) do
+    cached_from = DateTime.diff(DateTime.utc_now(), cached_at, :millisecond)
+    do_old_cache?(cached_from, cached_time)
+  end
 
-  def contains([next | users], username),
-    do: contains(users, username, next.username)
+  # Update user in given list.
+  def update(users, new_user) do
+    Enum.map(users, fn user ->
+      if user.username === new_user.username, do: new_user, else: user
+    end)
+  end
 
-  defp contains(_users, username, username),
-    do: true
+  defp do_find([]), do: nil
 
-  defp contains([], _username, _curr_username),
-    do: false
+  defp do_find([user]), do: user
 
-  defp contains([next | users], username, _curr_username),
-    do: contains(users, username, next.username)
+  defp do_old_cache?(cached_from, cache_time) when cached_from <= cache_time, do: true
+
+  defp do_old_cache?(cached_from, cache_time) when cached_from > cache_time, do: false
 end
--- homeworks/third_homework/users_cache/lib/users_cache.ex
+++ homeworks/third_homework/users_cache/lib/users_cache.ex
@@ -6,37 +6,59 @@ defmodule UsersCache do
   def start_link(),
     do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
 
-  def all(), do: GenServer.call(__MODULE__, :all)
-
-  def fetch(username) do
-    users = all()
-    fetch(User.contains(users, username), username)
-  end
-
-  defp fetch(true, username),
-    do: "User #{username} already cached."
-
-  defp fetch(false, username),
-    do: GenServer.cast(__MODULE__, {:fetch, username})
-
   def init(args), do: {:ok, args}
 
-  def handle_call(:all, _from, users),
+  def fetch_user(username, cached_time \\ 360_000) do
+    all_users = get_all_users()
+    curr_user = User.find(all_users, username)
+    do_fetch_user(curr_user, username, cached_time)
+  end
+
+  def get_all_users(), do: GenServer.call(__MODULE__, :get_all_users)
+
+  def handle_call(:get_all_users, _from, users),
     do: {:reply, users, users}
 
-  def handle_cast({:fetch, username}, users_cache) do
+  def handle_cast({:get_user, username}, users_cache) do
+    user = get_user(username)
+    {:noreply, [user | users_cache]}
+  end
+
+  def handle_cast({:update_user, username}, users_cache) do
+    user = get_user(username)
+    new_users_cache = User.update(users_cache, user)
+    {:noreply, new_users_cache}
+  end
+
+  defp do_fetch_user(true, username) do
+    GenServer.cast(__MODULE__, {:update_user, username})
+  end
+
+  defp do_fetch_user(false, username) do
+    "User #{username} already cached."
+  end
+
+  defp do_fetch_user(nil, username, _cached_time) do
+    GenServer.cast(__MODULE__, {:get_user, username})
+  end
+
+  defp do_fetch_user(user, username, cached_time) do
+    is_cache_old = User.old_cache?(user, cached_time)
+    do_fetch_user(is_cache_old, username)
+  end
+
+  defp get_user(username) do
     {:ok, response} = HTTPoison.get(@url <> username)
     {:ok, user} = Jason.decode(response.body)
 
-    new_user = %User{
+    %User{
       username: username,
       avatar_url: user["avatar_url"],
       bio: user["bio"],
       created_at: user["created_at"],
       email: user["email"],
-      id: user["id"]
+      id: user["id"],
+      cached_at: DateTime.utc_now()
     }
-
-    {:noreply, [new_user | users_cache]}
   end
 end
